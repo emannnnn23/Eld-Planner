a8e0b6fe224d7b922e5a09fc3c8bdee82ec6ef40
emannnnn23 <ymbongemanuel@gmail.com>
2026-09-10 23:32:14 +0800

    Add root health check route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from trips.views import health_check_view
 
 urlpatterns = [
+    path('', health_check_view, name='root_health'),
     path('admin/', admin.site.urls),
     path('api/', include('trips.urls')),
 ]
